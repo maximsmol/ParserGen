7ab42baa30abe17645384a53b6296a18ae87ee60
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-01-08 09:30:59 -0800

    explain fi and fo algorithms

--- src/alg/ll1/firstSets.js
+++ src/alg/ll1/firstSets.js
@@ -3,17 +3,20 @@ import {TokenMap} from '../../util/TokenMap';
 
 import {partEq, epsilon} from './grammarTools';
 
+// fi = first set
+// fi of `x` - set of every t that a match of `x` can start with
 export const firstSets = g => {
-  const fiWs = [];
-  const fiAs = new Map();
+  // maps: x -> fi + what caused each addition to fi
+  const fiWs = []; // rules
+  const fiAs = new Map(); // nts
 
   for (let i = 0; i < g.rules.length; ++i)
     fiWs.push(new TokenMap());
   for (const nt of g.nts)
     fiAs.set(nt, new TokenMap());
 
-  let changed = false;
-  do {
+  let changed = true;
+  while (changed) {
     changed = false;
     for (let i = 0; i < g.rules.length; ++i) {
       const rule = g.rules[i];
@@ -21,6 +24,9 @@ export const firstSets = g => {
       const fiW = fiWs[i];
       const fiA = fiAs.get(rule.nt);
 
+      // add t `x` to fi `set`
+      // add meta to causes of `x`
+      // update changed
       const add = (set, x, meta) => {
         let m = set.get(x);
         if (m == null) {
@@ -34,39 +40,52 @@ export const firstSets = g => {
         }
       };
 
+      // in subs of `rule`:
+      // for every skipable `x`:
+      //   assuming that `x` is matched, any of fi of `x` can be the start of a match
+      //   assuming that `x` is skipped: consider following subs
+      // when reached any non-skipable sub `x`, stop
+      //   because no following sub `y` can be reached without matching `x`,
+      //   making `x` the last possible first t in any match
+      // `x` is no-skipable:
+      //   if `x` is a t
+      //   if `x` is a nt that cannot start with epsilon
+      // if no non-skipable sub exists, `rule` is skipable, `rule.nt` is skipable
       for (let subI = 0; ; ++subI) {
         const addFiW = (set, x, nt, type) => add(set, x, {nt, subI, type});
 
-        if (subI === rule.sub.length) {
+        if (subI === rule.sub.length) { // no non-skipable sub
           addFiW(fiW, epsilon, null, 'end');
           break;
         }
 
         const sub = rule.sub[subI];
 
-        if (sub['?'] !== 'ref') {
+        if (sub['?'] !== 'ref') { // sub is t – non-skipable
           addFiW(fiW, sub, null, 't');
 
           break;
         }
+        // sub is nt
 
         const fiSubA = fiAs.get(sub.x);
 
-        for (const [fi,] of fiSubA) {
+        for (const [fi,] of fiSubA) { // assume nt is matched, add all of fi of nt
           if (partEq(fi, epsilon))
             continue;
           addFiW(fiW, fi, sub.x, 'fiA');
         }
 
-        if (!fiSubA.has(epsilon))
+        if (!fiSubA.has(epsilon)) // fi of nt has no epsilon – non-skipable
           break;
       }
 
+      // fi of every rule of nt is subset of fi of nt
       const addFiA = (set, x) => add(set, x, {i});
       for (const [fi,] of fiW)
         addFiA(fiA, fi);
     }
-  } while (changed);
+  }
 
   return {fiWs, fiAs};
 };
--- src/alg/ll1/followSet.js
+++ src/alg/ll1/followSet.js
@@ -3,16 +3,22 @@ import {TokenMap} from '../../util/TokenMap';
 
 import {partEq, epsilon} from './grammarTools';
 
+// fo = follow set
+// fo of nt `x` - set of every `t` that can follow a match of `x`
 export const followSet = (g, fiAs) => {
+  // map: nt -> fo + what caused each addition to fo
   const foAs = new Map();
 
   for (const nt of g.nts)
     foAs.set(nt, new TokenMap());
 
-  let changed = false;
-  do {
+  let changed = true;
+  while (changed) {
     changed = false;
 
+    // add t `x` to fo `set`
+    // add meta to causes of `x`
+    // update changed
     const add = (set, x, meta) => {
       let m = set.get(x);
       if (m == null) {
@@ -21,26 +27,43 @@ export const followSet = (g, fiAs) => {
         set.set(x, m);
       }
       if (!m.has(meta)) {
-        // changed = true;
+        // changed = true; // todo: ???
         m.add(meta);
       }
     };
 
+    // for all rules `r`:
+    // for all nts `A` in `r`:
+    // in subs following `A`:
+    // for every skipable `x`:
+    //   assuming that `x` is matched, each of fi of `x` can follow `A`
+    //   assuming that `x` is skipped: consider the following subs
+    // when reached non-skipable `x`, stop
+    //   because no following sub `y` can be reached without matching `x`
+    //   making `x` the beginning of every possible match that reached `x`
+    //   making `x` the last t that can follow `A`
+    // `x` is no-skipable:
+    //   if `x` is a t
+    //   if `x` is a nt that cannot start with epsilon
+    // if no non-skipable sub exists, each of fo of `rule.nt` can follow `A`,
+    //   because `A` is the last nt of `rule`,
+    //   making it precede anything that follows `rule.nt`, when matched by `rule`
     for (let i = 0; i < g.rules.length; ++i) {
       const rule = g.rules[i];
 
       for (let subI = 0; subI < rule.sub.length; ++subI) {
         const sub = rule.sub[subI];
 
-        if (sub['?'] !== 'ref')
+        if (sub['?'] !== 'ref') // don't collect fo of ts
           continue;
+        // sub is nt
 
         const foA = foAs.get(sub.x);
 
-        for (let x = subI+1; ; ++x) {
+        for (let x = subI+1; ; ++x) { // subs following `A`
           const addFoA = (set, x, nt, type) => add(set, x, {nt, i, subI, type});
 
-          if (x === rule.sub.length) {
+          if (x === rule.sub.length) { // no non-skipable sub
             // ignore the redundant assertion that
             // x can be followed by whatever can follow x
             if (rule.nt === sub.x)
@@ -49,7 +72,7 @@ export const followSet = (g, fiAs) => {
 
             const foArule = foAs.get(rule.nt);
 
-            for (const [fo,] of foArule)
+            for (const [fo,] of foArule) // add all of fo of `rule.nt` to fo of `A`
               addFoA(foA, fo, rule.nt, 'foA');
 
             break;
@@ -57,24 +80,26 @@ export const followSet = (g, fiAs) => {
 
           const subX = rule.sub[x];
 
-          if (subX['?'] !== 'ref') {
+          if (subX['?'] !== 'ref') { // subX is t – non-skipable
             addFoA(foA, subX, null, 't');
             break;
           }
+          // subX is nt
+
           const fiAx = fiAs.get(subX.x);
 
-          for (const [fi,] of fiAx) {
+          for (const [fi,] of fiAx) { // each of fi of `x` can follow `A`
             if (partEq(fi, epsilon))
               continue;
             addFoA(foA, fi, subX.x, 'fiA');
           }
 
-          if (!fiAx.has(epsilon))
+          if (!fiAx.has(epsilon)) // fi of `x` has no epsilon – non-skipable
             break;
         }
       }
     }
-  } while (changed);
+  }
 
   return foAs;
 };
