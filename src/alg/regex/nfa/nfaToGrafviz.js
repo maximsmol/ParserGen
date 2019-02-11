2cfe1ae75e21833445b61c9535605b573c8b6905
Maksim Smolin <maximsmol@gmail.com>
2019-02-11 12:28:35 -0800

    add NFA evaluator

--- src/alg/regex/nfa/nfaToGrafviz.js
+++ src/alg/regex/nfa/nfaToGrafviz.js
@@ -1,12 +1,18 @@
 import {renderArrow} from './thompson';
 
-export const nfaToGrafviz = (nfa) => {
+export const nfaToGrafvizBody = (nfa) => {
   const res = [];
-  res.push('digraph nfa {');
   for (let i = 0; i < nfa.length; ++i)
     for (const a of nfa[i]) {
       res.push(`  ${i} -> ${a.f} [label = "${renderArrow(a).replace(/\\([^\\"])/g, '\\\\$1')}"];`);
     }
+  return res;
+};
+
+export const nfaToGrafviz = (nfa) => {
+  let res = [];
+  res.push('digraph nfa {');
+  res = res.concat(nfaToGrafvizBody(nfa));
   res.push('}');
   return res.join('\n');
 };
