7c3d915e51fad65770e052bfca4148b494db5be7
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-04-11 13:15:42 -0700

    add sourcemapping to regex parsing

--- src/alg/regex/regexStrAST_renderer.js
+++ src/alg/regex/regexStrAST_renderer.js
@@ -20,3 +20,54 @@ export const renderNode = (node) => {
 
   throw new Error('unsupported');
 };
+
+const renderASTSource1 = (node, source, indentStr) => {
+  const mapping = node.sourceMapInfo;
+
+  if (mapping == null)
+    throw new Error(`mapping invalid in ${renderNode(node)}`);
+
+  if (mapping.range.start == null)
+    throw new Error(`mapping has no start in ${renderNode(node)}`);
+  if (mapping.range.end == null)
+    throw new Error(`mapping has no end in ${renderNode(node)}`);
+
+  const res = [];
+  res.push(indentStr+source.substring(mapping.range.start, mapping.range.end));
+  if (mapping.anchor != null)
+    res.push(indentStr+' '.repeat(mapping.anchor-mapping.range.start)+'^');
+  return res;
+};
+export const renderASTSource = (node, source) => renderASTSource1(node, source, '');
+export const printASTSource = (node, source) => console.log(renderASTSource(node, source).join('\n'));
+
+const renderASTTree1 = (node, source, indentStr) => {
+  const nextIndentStr = indentStr + '  ';
+
+  if (['.', '&', 'char'].includes(node['?']))
+    return renderASTSource1(node, source, indentStr);
+  if (['[]', '()'].includes(node['?'])) {
+    const res = [];
+    res.push(...renderASTSource1(node, source, indentStr));
+    for (let i = 0; i < node.x.length; ++i)
+      res.push(...renderASTTree1(node.x[i], source, nextIndentStr));
+    return res;
+  }
+  if (['|', 'a-b'].includes(node['?'])) {
+    const res = [];
+    res.push(...renderASTSource1(node, source, indentStr));
+    res.push(...renderASTTree1(node.a, source, nextIndentStr));
+    res.push(...renderASTTree1(node.b, source, nextIndentStr));
+    return res;
+  }
+  if (['*', '+', '?', '{n}', '{a,b}'].includes(node['?'])) {
+    const res = [];
+    res.push(...renderASTSource1(node, source, indentStr));
+    res.push(...renderASTTree1(node.x, source, nextIndentStr));
+    return res;
+  }
+
+  return [indentStr+`can't render a node of type ${node['?']}`];
+};
+export const renderASTTree = (node, source) => renderASTTree1(node, source, '');
+export const printASTTree = (node, source) => console.log(renderASTTree(node, source).join('\n'));
